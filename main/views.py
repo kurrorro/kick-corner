405b338f8fcd5fafb58c680b7510acd73eab5df4
kurrorro <keishavnia@gmail.com>
2025-09-17 15:55:14 +0700

    Tugas Individu 4 - user register, login. logout, and cookies session

--- main/views.py
+++ main/views.py
@@ -1,18 +1,32 @@
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.core import serializers
 from django.shortcuts import render, redirect, get_object_or_404
 from django.shortcuts import render
 from main.models import Product
 from main.forms import ProductForm
+import datetime
 
+@login_required(login_url='/login')
 def show_main(request):
-    products = Product.objects.all()  # ambil semua produk
+    filter_type = request.GET.get("filter", "all")  # default 'all'
+
+    if filter_type == "all":
+        products = Product.objects.all()
+    else:
+        products = Product.objects.filter(user=request.user)
+        
     context = {
         'shop': 'Kick Corner',
         'npm': '2406437331',
         'name': 'Keisha Vania Laurent',
         'class': 'PBP B',
         'products': products, 
+        'last_login': request.COOKIES.get('last_login', 'Never'),
     }
 
     return render(request, "main.html", context)
@@ -20,13 +34,19 @@ def show_main(request):
 def add_product(request): # menghasilkan form yang dapat menambahkan data Product secara otomatis ketika data di-submit dari form
     form = ProductForm(request.POST or None)
 
-    if form.is_valid() and request.method == "POST":
-        form.save()
+    if form.is_valid() and request.method == 'POST':
+        product_entry = form.save(commit = False)
+        product_entry.user = request.user
+        product_entry.save()
         return redirect('main:show_main')
 
-    context = {'form': form}
+    context = {
+        'form': form
+    }
+
     return render(request, "add_product.html", context)
 
+@login_required(login_url='/login')
 def show_product(request, id):
     product = get_object_or_404(Product, pk=id) # untuk mengambil objek News berdasarkan primary key (id). Jika objek tidak ditemukan, akan mengembalikan halaman 404
     product.increment_views() # menambah jumlah views
@@ -61,3 +81,37 @@ def show_json_by_id(request, product_id):
         return HttpResponse(json_data, content_type="application/json")
     except Product.DoesNotExist:
        return HttpResponse(status=404)
+    
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+
+    else:
+       form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
